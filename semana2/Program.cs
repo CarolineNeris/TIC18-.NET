027ef786c53292efcfd4943707c83de87d1f3a2c
Carol <carolnerisfonseca@gmail.com>
2023-11-25 08:48:37 -0300

    tentando outra abordagem

--- semana2/Program.cs
+++ semana2/Program.cs
@@ -1,101 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using GerenciadorTarefas;
 
 class Program
 {
-    static List<Task> tasks = new List<Task>();
-
-    static void Main()
+    static void Main(string[] args)
     {
-        while (true)
+        GerenciadorTarefas.GerenciadorTarefas gerenciador = new GerenciadorTarefas.GerenciadorTarefas();
+
+        int opcao = 0;
+        do
         {
+            Console.WriteLine("------ Menu ------");
             Console.WriteLine("1. Criar Tarefa");
-            Console.WriteLine("2. Listar Todas as Tarefas");
-            Console.WriteLine("3. Marcar Tarefa como Concluída");
-            Console.WriteLine("4. Listar Tarefas Pendentes");
-            Console.WriteLine("5. Listar Tarefas Concluídas");
-            Console.WriteLine("6. Excluir Tarefa");
-            Console.WriteLine("7. Pesquisar Tarefa por Palavra-Chave");
-            Console.WriteLine("8. Exibir Estatísticas");
-            Console.WriteLine("9. Sair");
-
-            int opcao = int.Parse(Console.ReadLine());
-
-            switch (opcao)
+            Console.Write("Escolha uma opção: ");
+            if (int.TryParse(Console.ReadLine(), out opcao))
             {
-                case 1:
-                    CriarTarefa();
-                    break;
-                case 2:
-                    ListarTodasAsTarefas();
-                    break;
-                case 3:
-                    MarcarTarefaComoConcluida();
-                    break;
-                case 4:
-                    ListarTarefasPendentes();
-                    break;
-                case 5:
-                    ListarTarefasConcluidas();
-                    break;
-                case 6:
-                    ExcluirTarefa();
-                    break;
-                case 7:
-                    PesquisarPorPalavraChave();
-                    break;
-                case 8:
-                    ExibirEstatisticas();
-                    break;
-                case 9:
-                    return;
-                default:
-                    Console.WriteLine("Opção inválida. Tente novamente.");
-                    break;
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o título da tarefa:");
+                        string titulo = Console.ReadLine();
+                        Console.WriteLine("Digite a descrição da tarefa:");
+                        string descricao = Console.ReadLine();
+                        Console.WriteLine("Digite a data de vencimento da tarefa (ano-mês-dia):");
+                        DateTime dataVencimento;
+                        DateTime.TryParse(Console.ReadLine(), out dataVencimento);
+                        gerenciador.CriarTarefa(titulo, descricao, dataVencimento);
+                        break;
+                    case 9:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Escolha novamente.");
+                        break;
+                }
             }
-        }
-    }
+            else
+            {
+                Console.WriteLine("Opção inválida. Escolha novamente.");
+            }
 
-    static void CriarTarefa()
-    {
-        Console.WriteLine("Digite o título da tarefa:");
-        string titulo = Console.ReadLine();
-
-        Console.WriteLine("Digite a descrição da tarefa:");
-        string descricao = Console.ReadLine();
-
-        Console.WriteLine("Digite a data de vencimento (DD/MM/AAAA):");
-        DateTime dataVencimento = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
-
-        Task novaTarefa = new Task(titulo, descricao, dataVencimento);
-        tasks.Add(novaTarefa);
-        Console.WriteLine("Tarefa criada com sucesso!");
-    }
-
-    static void ListarTodasAsTarefas()
-    {
-        foreach (var task in tasks)
-        {
-            Console.WriteLine($"Título: {task.Titulo} - Descrição: {task.Descricao} - Data de Vencimento: {task.DataVencimento.ToShortDateString()} - Concluída: {(task.Concluida ? "Sim" : "Não")}");
-        }
-    }
-
-    // Implemente as outras funções conforme necessário (MarcarTarefaComoConcluida, ListarTarefasPendentes, ListarTarefasConcluidas, ExcluirTarefa, PesquisarPorPalavraChave, ExibirEstatisticas)
-}
-
-class Task
-{
-    public string Titulo { get; }
-    public string Descricao { get; }
-    public DateTime DataVencimento { get; }
-    public bool Concluida { get; set; }
-
-    public Task(string titulo, string descricao, DateTime dataVencimento)
-    {
-        Titulo = titulo;
-        Descricao = descricao;
-        DataVencimento = dataVencimento;
-        Concluida = false;
+        } while (opcao != 9);
     }
 }
